067d2f8a240a0c0631e9945a39d167a981838df6
yogev-namir <yogevnamir97@gmail.com>
2024-06-09 16:18:35 +0300

    files names changed

---
+++ RunningScripts/clone_and_init_yogev-avishag.sh
@@ -0,0 +1,10 @@
+# cloning your code from github:
+git clone https://github.com/yogev-namir/HumanChoicePrediction.git
+
+# Your main sweep:
+python final_sweep_YOUR_UID.py
+
+# More runs appear in your report:
+# python sweep_1.py
+# python sweep_2.py
+# python sweep_3.py
---
+++ RunningScripts/final_sweep_yogev-avishag.py
@@ -0,0 +1,33 @@
+import wandb
+
+YOUR_WANDB_USERNAME = "yogev-avishag"
+project = "NLP2024_PROJECT_yogev-avishag"
+
+command = [
+        "${ENVIRONMENT_VARIABLE}",
+        "${interpreter}",
+        "StrategyTransfer.py",
+        "${project}",
+        "${args}"
+    ]
+
+sweep_config = {
+    "name": "Final Sweep Submission",
+    "method": "grid",
+    "metric": {
+        "goal": "maximize",
+        "name": "AUC.test.max"
+    },
+    "parameters": {
+        "ENV_HPT_mode": {"values": [False]},
+        "architecture": {"values": ["LSTM"]},
+        "seed": {"values": [1]},
+        "features": {"values": ["EFs"]},
+    },
+    "command": command
+}
+
+# Initialize a new sweep
+sweep_id = wandb.sweep(sweep=sweep_config, project=project)
+print("run this line to run your agent in a screen:")
+print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
